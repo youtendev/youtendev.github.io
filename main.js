21e45743dc38e3c909bdabb98da7b39e5507aff2
uttmm <uthen22th@gmail.com>
2021-07-12 14:56:17 +0700

    update js

---
+++ main.js
@@ -0,0 +1,28 @@
+$(document).ready(() => {
+  $("#open-discord").click(() => {
+    openDiscord()
+  })
+
+  $("#close-btn").click(() => {
+    closeModal()
+  })
+  $("#close-btn").hover(() => {
+    $("#close-btn").addClass("rotate-in")
+    $("#close-btn").removeClass("rotate-out")
+  })
+  $("#close-btn").mouseleave(() => {
+    $("#close-btn").addClass("rotate-out")
+    $("#close-btn").removeClass("rotate-in")
+  })
+
+  const openDiscord = () => {
+    $("#modal").css("display", "block")
+    $("#modal-content").html(
+      "<h1 class='modal-title'>ลิงค์เข้า Discord</h1><p class='modal-details'>" +
+        "<a href='https://discord.gg/6NAcF39J' target='_blank'rel='noopener noreferrer' class='modal-link-btn'>Discord</a></p>"
+    )
+  }
+  const closeModal = () => {
+    $("#modal").css("display", "none")
+  }
+})
