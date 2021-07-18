c9bec2ebffae6a988e94f7f337c0b8a9fbaf24a9
uttmm <uthen22th@gmail.com>
2021-07-18 20:07:53 +0700

    update index date table

--- main.js
+++ main.js
@@ -6,24 +6,29 @@ $(document).ready(() => {
   $("#close-btn").click(() => {
     closeModal()
   })
-  $("#close-btn").hover(() => {
-    $("#close-btn").addClass("rotate-in")
-    $("#close-btn").removeClass("rotate-out")
-  })
-  $("#close-btn").mouseleave(() => {
-    $("#close-btn").addClass("rotate-out")
-    $("#close-btn").removeClass("rotate-in")
-  })
+  //   $("#close-btn").hover(() => {
+  //     $("#close-btn").addClass("rotate-in")
+  //     $("#close-btn").removeClass("rotate-out")
+  //   })
+  //   $("#close-btn").mouseleave(() => {
+  //     $("#close-btn").addClass("rotate-out")
+  //     $("#close-btn").removeClass("rotate-in")
+  //   })
 
   openDiscord = () => {
+    $(".scale-ver-top").css("animation-name", "scale-in-ver-top")
+    $(".scale-ver-top").css("-webkit-animation-name", "scale-in-ver-top")
     $("#modal").css("display", "block")
+
     $("#modal-content").html(
       "<h1 class='modal-title fade-in'>ลิงค์เข้า Discord</h1><p class='modal-details'>" +
         "<a href='https://discord.gg/6NAcF39J' target='_blank'rel='noopener noreferrer' class='modal-link-btn fade-in'>Discord ปี5</a>" +
         "<a href='https://discord.gg/6NAcF39J' target='_blank'rel='noopener noreferrer' class='modal-link-btn fade-in'>Discord ปี3</a></p>"
     )
   }
-  const closeModal = () => {
-    $("#modal").css("display", "none")
+  closeModal = () => {
+    $(".scale-ver-top").css("animation-name", "scale-out-ver-top")
+    $(".scale-ver-top").css("-webkit-animation-name", "scale-out-ver-top")
+    // $("#modal").css("display", "none")
   }
 })
