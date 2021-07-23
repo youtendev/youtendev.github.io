6aadfe44f925cf9e6d5e48fb1bf24c71b029b285
uttmm <uthen22th@gmail.com>
2021-07-23 10:05:51 +0700

    update link work

--- main.js
+++ main.js
@@ -38,7 +38,7 @@ $(document).ready(() => {
 
     $("#modal-content").html(
       "<h1 class='modal-title fade-in'>ผลลงานนักศึกษา</h1><p class='modal-details'>" +
-        "<a href='./hello.html' target='_blank'rel='noopener noreferrer' class='modal-link-btn fade-in'>กลุ่มที่ 1</a>" +
+        "<a href='https://ursulabaconner.github.io/' target='_blank'rel='noopener noreferrer' class='modal-link-btn fade-in'>กลุ่มที่ 1</a>" +
         "<a href='#' target='_blank'rel='noopener noreferrer' class='modal-link-btn fade-in'>กลุ่มที่ 2</a>" +
         "<a href='#' target='_blank'rel='noopener noreferrer' class='modal-link-btn fade-in'>กลุ่มที่ 3</a></p>"
     )
